d78e3389ef81dc0dc551ebfd953afe30ff0af296
Sajal687 <78894677+Sajal687@users.noreply.github.com>
2021-10-15 01:02:30 +0530

    Pattern Problem Code File & Solution

---
+++ pattern problem codefile.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+int main()
+{
+  int i,j,n;
+  cout<<"Enter Number Of Rows : ";
+  cin>>n;
+  for(i=0;i<n;i++)
+ {
+    for(j=0;j<=i;j++)
+    {
+      if((i-j)%2==0)
+        cout<<"1";
+      else
+        cout<<"0";
+    }
+    cout<<"\n";
+  }
+}
